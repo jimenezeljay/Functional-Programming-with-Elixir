1dd1b9079ffb33cd7336c7974d2f1afff66a70e6
jimenezeljay <143433352+jimenezeljay@users.noreply.github.com>
2026-03-16 17:39:41 -0400

    learned module attributes

--- example/lib/example.ex
+++ example/lib/example.ex
@@ -1,14 +1,17 @@
 defmodule Example do
   use Application
-  alias UUID #how you export external libraries to your code, you can use the alias keyword to create an alias for the module. In this case, it allows you to refer to the UUID module without having to prefix it with its full name.
+
+  @x 5
 
   def start(_type, _args) do #_args describes data past to the parameter
     #code
-    IO.puts(UUID.uuid4()) # generates a random UUID (Universally Unique Identifier) using the uuid4 function from the UUID module. This is often used to create unique identifiers for various purposes, such as database records or session tokens.
+    Example.main()
     Supervisor.start_link([], strategy: :one_for_one ) #it supervises other processes (child processes) and restarts them if they fail. The empty list [] indicates that there are no child processes to supervise, and the strategy :one_for_one means that if a child process crashes, only that process will be restarted
   end
 
-  def hello do
-    :world
+  def main do
+    IO.puts(@x)
   end
 end
+
+#creating variables
