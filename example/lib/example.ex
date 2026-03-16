040c8e67987f1619724246363e5f09eeea4b11d0
jimenezeljay <143433352+jimenezeljay@users.noreply.github.com>
2026-03-16 17:23:22 -0400

    intro into hexs

--- example/lib/example.ex
+++ example/lib/example.ex
@@ -1,8 +1,10 @@
 defmodule Example do
   use Application
-  def start(_type, _args) do # _args describes data past to the parameter
+  alias UUID #how you export external libraries to your code, you can use the alias keyword to create an alias for the module. In this case, it allows you to refer to the UUID module without having to prefix it with its full name.
+
+  def start(_type, _args) do #_args describes data past to the parameter
     #code
-    IO.puts(Example.hello())
+    IO.puts(UUID.uuid4()) # generates a random UUID (Universally Unique Identifier) using the uuid4 function from the UUID module. This is often used to create unique identifiers for various purposes, such as database records or session tokens.
     Supervisor.start_link([], strategy: :one_for_one ) #it supervises other processes (child processes) and restarts them if they fail. The empty list [] indicates that there are no child processes to supervise, and the strategy :one_for_one means that if a child process crashes, only that process will be restarted
   end
 
