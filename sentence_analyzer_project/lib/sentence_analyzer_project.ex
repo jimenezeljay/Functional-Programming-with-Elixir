026218efe89a8f604510adbe698a464c2a8c9466
jimenezeljay <143433352+jimenezeljay@users.noreply.github.com>
2026-03-20 10:39:48 -0400

    made changes to the count function to account for how many sentences

--- sentence_analyzer_project/lib/sentence_analyzer_project.ex
+++ sentence_analyzer_project/lib/sentence_analyzer_project.ex
@@ -4,7 +4,7 @@ defmodule SentenceAnalyzerProject do
   """
   #entry point for the IO.puts to be
   def main do
-   text = "Hello, world! This is a test sentence. How many words are here?"
+   text = "Hello, world! This is a test sentence. How many words are here? Word Word Word Word"
 
    #word count
    wc = count_words(text)
@@ -21,10 +21,22 @@ defmodule SentenceAnalyzerProject do
 
 
 #pure functions
-  def count_words(text) do
+
+  #this converts string to list first and then counts the words using recursion, which is more in line with functional programming principles
+  def count_words(text) when is_binary(text) do
     text
     |> String.split()
-    |> length()
+    |> count_words()
+  end
+
+  #base case where empty the list = 0 words
+  def count_words([]) do
+    0
+  end
+
+  # recursive case where we take the head of the list and add 1 for each word, then call count_words on the tail of the list
+  def count_words([_head | tail]) do
+    1 + count_words(tail)
   end
 
   #regex pattern to split sentences based on punctuation marks
@@ -35,8 +47,18 @@ defmodule SentenceAnalyzerProject do
     |> length()
   end
 
-  def top_words(_text) do
-    #TODO
+  #top words function
+  def top_words(text) do
+    text
+    |> String.downcase() # goes from something like "Hello" to "hello"
+    |> String.split() # splits into a word list
+    |> Enum.map(fn word ->
+      String.replace(word, ~r/[^a-z]/, "") # removes punctuation
+      end)
+    |> Enum.filter( fn word -> word != "" end) # remove empty strings
+    |> Enum.frequencies() # %{"hello" => 2, "world => 1, ...}
+    |> Enum.sort_by(fn {_word, count} -> count end, :desc) # sort by the count in descending order
+    |> Enum.take(5) # take the top 5 words
   end
 end
 
